7fe0dbcd7c07573d1a6c48d7d0025bb1cfbb9ca5
naveenneerudi1475@gmail.com <naveenneerudi1475@gmail.com>
2026-01-31 19:45:06 +0530

    assignment 1 submission

---
+++ Task 1.py
@@ -0,0 +1,15 @@
+# This python program performs Addition,Substraction,Multiplication,Division of a two numbers
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+
+c=a+b # computing the addition of two numbers
+d=a-b # computing the substraction of two numbers
+e=a*b # computing the multiplication of two numbers
+f=a/b # computing the division of the two numbers
+
+
+print("Addition:",c)
+print ("Substraction:",d)
+print("Multiplication:",e)
+print("Division:",f)
